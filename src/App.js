93cbf40ed719d1579df285505d5a0b49c7932ef3
Purushottam Deshpande <purushottam.deshpande@clarivate.com>
2021-03-14 18:59:10 +0530

    Base React app + material UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Movie Booking</h1>
+        <div>
+          <Button variant='contained' color='primary'>Hello World</Button>
+        </div>
       </header>
     </div>
   );
